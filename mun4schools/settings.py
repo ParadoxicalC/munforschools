1e10e1bc14678070aa49a43271614acd9ae277f0
Sukir <sukirtc@gmail.com>
2025-07-04 11:54:58 +0530

    emails now work

--- mun4schools/settings.py
+++ mun4schools/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your-app-password'
+EMAIL_HOST_USER = 'sukirtc@gmail.com'
+EMAIL_HOST_PASSWORD = 'jsxz xqwy tkro gtod'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
