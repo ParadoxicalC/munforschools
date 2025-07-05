e064cbfdeed45bb64670166a1396fb17f1f9b341
Agilan <agilansevvel@gmail.com>
2025-07-05 23:13:53 +0530

    final stages and updated data

--- mun4schools/settings.py
+++ mun4schools/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sukirtc@gmail.com'
-EMAIL_HOST_PASSWORD = 'jsxz xqwy tkro gtod'
+EMAIL_HOST_USER = 'mun4schools.tipse@gmail.com'
+EMAIL_HOST_PASSWORD = 'zrph hjmf omhh slks'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
